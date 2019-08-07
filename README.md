47768dc3f3fb1e68d60b731301e6f25c957b423f
konda sraddha <k.sraddhareddy@gmail.com>
2019-08-07 16:32:27 +0530

    TrackByNameQuery

---
+++ README.md
@@ -0,0 +1,2 @@
+# BootTasks
+Add an endpoint to search trackByName. Understand @Query and parameter passing to @Query
---
+++ src/main/java/com/stackroute/Muzix/repository/TrackRepository.java
@@ -0,0 +1,14 @@
+package com.stackroute.Muzix.repository;
+
+import com.stackroute.Muzix.domain.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track,Integer> {
+    @Query("from Track where trackName=?1")
+    public List<Track> findByTrackName(String trackName);
+}
