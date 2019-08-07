8ed8a3eb3a48fcf05c245b0b221b8c60a60ad133
konda sraddha <k.sraddhareddy@gmail.com>
2019-08-07 16:16:32 +0530

    Exceptions task

---
+++ README.md
@@ -0,0 +1,4 @@
+# SpringBootTasks
+5. Create custom exceptions TrackNotFoundException, TrackAlreadyExistsException in a
+com.stack....exceptions package. Perform appropriate exception handling and propagation
+back.
---
+++ src/main/java/com/stackroute/Muzix/service/TrackService.java
@@ -0,0 +1,16 @@
+package com.stackroute.Muzix.service;
+
+import com.stackroute.Muzix.domain.Track;
+import com.stackroute.Muzix.exception.TrackAlreadyExistsException;
+import com.stackroute.Muzix.exception.TrackNotFoundException;
+
+import java.util.List;
+
+public interface TrackService {
+    public Track saveTrack(Track track) throws TrackAlreadyExistsException;
+    public List<Track> getAllTracks() throws TrackNotFoundException;
+    public Track updateTrack(Track track) throws TrackNotFoundException;
+    public Track deleteTrack(int id) throws TrackNotFoundException;
+    public Track getTrackById(int id) throws TrackNotFoundException;
+    public List<Track> findByTrackName(String name) throws TrackNotFoundException;
+}
