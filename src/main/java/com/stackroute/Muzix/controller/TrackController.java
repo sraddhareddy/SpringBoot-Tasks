bffe9c35a2dd33390a63dd595a0eac3957edd1ff
sraddhareddy <k.sraddhareddy@gmail.com>
2019-08-08 11:14:33 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/Muzix/controller/TrackController.java
+++ src/main/java/com/stackroute/Muzix/controller/TrackController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class TrackController {
 
     private TrackService trackService;
-
+@Autowired
     public TrackController(TrackService trackService) {
         this.trackService = trackService;
     }
